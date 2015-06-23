7f08c4742c3d342abe79901051caed380775d6ea
shri11 <sreesha011@gmail.com>
2015-06-23 23:53:11 +0400

    Error resolved and name added

--- FirstRun/app/src/main/java/com/example/sreesha/firstrun/SplashScreen.java
+++ FirstRun/app/src/main/java/com/example/sreesha/firstrun/SplashScreen.java
@@ -1,10 +1,7 @@
 package com.example.sreesha.firstrun;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
